749c631bd0c97c53fe02f614dd9c344da59a520f
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-03-18 11:26:18 +0300

    Ref parameter

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
 Console.WriteLine("Hello, World!");
 
 SayHi("Mahmut");
 
+int number = 2;
+MultiplyValueByTwo(ref number);
+
+
 void SayHi(string name)
 {
     Console.WriteLine("Hi " + name + "!");
+}
+
+
+void MultiplyValueByTwo(ref int number)
+{
+    number = number * 2;
 }
